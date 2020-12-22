e9163914bc70d8971af08bc2d07184050843160c
SLO42 <sam.olive.lee@gmail.com>
2020-12-22 11:29:22 -0600

    created checks for specfic key data

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+
 fetch('https://financialmodelingprep.com/api/v3/profile/AAPL\?apikey\=demo').then(body => body.json()).then(res => {
     const data = res[0];
     let html = "<ul> ";
     Object.keys(data).map(( key ) => {
-        html += `<li key=${data[key]}>${key} | ${data[key]} </li>`
+        if (key === "changes"){
+            if (data[key] > 0) {
+                html += `<li style="color:green" key=${key}>${key} | ${data[key]} </li>`
+            }
+            else {
+                html += `<li style="color:red" key=${key}>${key} | ${data[key]} </li>`
+            }
+        }
+        else if (key === "price"){
+            html += `<li key=${key}>${key} | $${data[key]} </li>`
+        }
+        else if (key === "website"){
+            html += `<li key=${key}>${key} |<a href="${data[key]}" target="_blank"> ${data[key]} </a> </li>`
+            
+        }
+        else html += `<li key=${key}>${key} | ${data[key]} </li>`
     });
     html += "</ul>";
-
-    document.body.innerHTML = html;
+    return document.body.innerHTML = html;
 });
